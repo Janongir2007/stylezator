e8fd1f5ad3518f965a8b5fdc5fdefc0fe2b64201
Jahongir Sobirov <77984780+Janongir2007@users.noreply.github.com>
2021-02-27 19:51:49 +0500

    Create stylezator.js

---
+++ versions/1.0.0/stylezator.js
@@ -0,0 +1,48 @@
+/*
+    Author: Jahongir Sobirov
+    Licencse: MIT
+    Stylezator Group Inc 2021 All rights reserved
+    Stylezator UI framework
+*/
+function dropdown() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+var coll = document.getElementsByClassName("collapsible");
+var len;
+
+for (len = 0; len < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    } 
+  });
+}
+function openTab(evt, cityName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
